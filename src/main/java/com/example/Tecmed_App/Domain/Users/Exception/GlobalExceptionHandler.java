12562e59c467cb9fcaed7a8490237eb2efee8fa9
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 18:06:18 -0300

    feat: Implement global exception handling
    - Added `GlobalExceptionHandler` class annotated with `@ControllerAdvice` to handle various exceptions globally across the application.
    - Defined exception handlers for `UserAlreadyExistsException`, `AuthenticationException`, `UserValidationException`, and `IllegalArgumentException`.
    - Each handler returns an appropriate HTTP status code along with a descriptive error message in the response.
    - This enhances centralized error management and provides consistent feedback to API clients.

---
+++ src/main/java/com/example/Tecmed_App/Domain/Users/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.Tecmed_App.Domain.Users.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<String> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<String> handleAuthenticationException(AuthenticationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(UserValidationException.class)
+    public ResponseEntity<String> handleUserValidationException(UserValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
