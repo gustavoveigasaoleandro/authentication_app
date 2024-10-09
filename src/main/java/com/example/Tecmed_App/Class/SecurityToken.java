2e87f8b54609bc7f70d5b5f91d73da80c0b3a72a
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 17:27:31 -0300

    feat: Add SecurityToken class for managing JWT tokens
    - Created `SecurityToken` class to encapsulate JWT tokens.
    - Provided constructors for default and token-specific instantiation.
    - Included getter and setter methods for accessing and modifying the token.

---
+++ src/main/java/com/example/Tecmed_App/Class/SecurityToken.java
@@ -0,0 +1,20 @@
+package com.example.Tecmed_App.Class;
+
+public class SecurityToken {
+
+    private String token;
+
+    public SecurityToken() {}
+
+    public SecurityToken(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
