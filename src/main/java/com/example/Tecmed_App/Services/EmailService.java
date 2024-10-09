05d838131be58f85cae4d45300ab764945a7f11d
Gustavo Veiga São Leandro <55115090+guta307@users.noreply.github.com>
2024-10-09 18:05:07 -0300

    feat: Integrate dynamic email sender address in EmailService
    - Configured `EmailService` to dynamically read the sender's email from environment variables using `@Value("${email.from}")`.
    - Adjusted the email sender address in the `sendEmail` method to use the `fromEmail` value read from application properties.
    - Ensured that the email sender address is configurable via the `application.properties` or `application.yml` file.

--- src/main/java/com/example/Tecmed_App/Services/EmailService.java
+++ src/main/java/com/example/Tecmed_App/Services/EmailService.java
@@ -2,6 +2,7 @@ package com.example.Tecmed_App.Services;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,9 +13,12 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    // Lê o valor da variável de ambiente configurada no application.properties ou application.yml
+    @Value("${email.from}")
+    private String fromEmail;
     public void sendEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("veiga187@hotmail.com"); // Certifique-se de que o remetente é o e-mail corretos
+        message.setFrom(fromEmail);  // Certifique-se de que o remetente é o e-mail corretos
         message.setTo(to);
         message.setSubject(subject);
         message.setText(text);
